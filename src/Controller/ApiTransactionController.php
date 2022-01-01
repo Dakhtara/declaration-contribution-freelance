deae5f8ac6a1910db0a369b180c00b755e33e58e
anthony <anthony.matignon@gmail.com>
2022-01-01 15:20:56 +0100

    Add API to create a new transaction for a specific user

--- src/Controller/ApiTransactionController.php
+++ src/Controller/ApiTransactionController.php
@@ -2,25 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Transaction;
 use App\Manager\TransactionManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[IsGranted('ROLE_USER')]
+#[Route('/api/transactions', name: 'api_transactions_')]
 class ApiTransactionController extends AbstractController
 {
-    #[Route('/api/transactions', name: 'api_transactions')]
-    public function index(TransactionManager $transactionManager, Request $request): Response
+
+    public function __construct(protected TransactionManager $transactionManager,
+                                protected SerializerInterface $serializer
+    )
+    {
+    }
+
+    #[Route(methods: ['GET'], name: 'list')]
+    public function index(Request $request): Response
     {
         if (($date = $request->query->get('date')) !== null) {
-            $transactions = $transactionManager->getByUserAndTrimester($this->getUser(), new \DateTime($date));
+            $transactions = $this->transactionManager->getByUserAndTrimester($this->getUser(), new \DateTime($date));
         } else {
-            $transactions = $transactionManager->getByUser($this->getUser());
+            $transactions = $this->transactionManager->getByUser($this->getUser());
         }
 
         return $this->json($transactions, context: ['groups' => 'read:transaction']);
+    }
+
+    #[Route(name: 'new', methods: ['POST'])]
+    public function addTransaction(Request $request): JsonResponse
+    {
+        $transaction = $this->serializer->deserialize($request->getContent(), Transaction::class,'json');
+
+        $this->transactionManager->save($transaction);
+        $user = $this->getUser();
+
+        $user->addTransaction($transaction);
+
+        return $this->json($transaction, 201, context: ['groups' => 'read:transaction']);
     }
 }
--- tests/Controller/ApiTransactionTest.php
+++ tests/Controller/ApiTransactionTest.php
@@ -78,4 +78,18 @@ class ApiTransactionTest extends WebTestCase
         $this->assertIsString($content[0]['dateTime']);
         $this->assertIsNumeric($content[0]['price']);
     }
+
+    public function testAddTransaction(): void
+    {
+        $client = $this->createAuthenticatedClient('anthony.matignon@domain.com');
+        $client->request(
+            'POST',
+            '/api/transactions',
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: json_encode(['slices' => null, 'label' => 'Iphone 12', 'price' => 200, 'type' => 'credit', 'dateTime' => '2021-12-31'])
+        );
+
+        $content = json_decode($client->getResponse()->getContent(), true);
+        $this->assertSame(201, $client->getResponse()->getStatusCode());
+    }
 }
